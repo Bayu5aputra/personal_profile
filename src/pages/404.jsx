a8ef3ef91965dab27b4eb21d3339155b445d828d
Bayu Saputra <bayusaputra.005.003@gmail.com>
2025-11-18 10:38:21 +0700

    Resolving error

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
-import { faFaceSadTear } from "@fortawesome/free-regular-svg-icons";
+import { faHome, faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 
 import NavBar from "../components/common/navBar";
 import Logo from "../components/common/logo";
@@ -12,7 +12,7 @@ import "./styles/404.css";
 
 const Notfound = () => {
 	useEffect(() => {
-		document.title = `404 | ${INFO.main.title}`;
+		document.title = `404 - Page Not Found | ${INFO.main.title}`;
 	}, []);
 
 	return (
@@ -25,21 +25,61 @@ const Notfound = () => {
 							<Logo width={46} />
 						</div>
 					</div>
+					
 					<div className="notfound-container">
-						<div className="notfound-message">
-							<div className="notfound-title">
-								Oops! <FontAwesomeIcon icon={faFaceSadTear} />
+						<div className="notfound-background-text">404</div>
+						
+						<div className="notfound-content">
+							<div className="notfound-glitch-wrapper">
+								<div className="notfound-glitch" data-text="404">404</div>
 							</div>
-							<div className="not-found-message">
-								We can't seem to find the page you're looking
-								for.
+							
+							<h1 className="notfound-title">Page Not Found</h1>
+							
+							<p className="notfound-description">
+								Oops! The page you're looking for seems to have wandered off into the digital void.
 								<br />
-								The requested URL "{window.location.href}" was
-								not found on this server.
+								Don't worry, even the best networks have their dead ends.
+							</p>
+							
+							<div className="notfound-url">
+								<span className="notfound-url-label">Requested URL:</span>
+								<code className="notfound-url-text">{window.location.pathname}</code>
 							</div>
-							<a href="/" className="not-found-link">
-								Go back to the home page
-							</a>
+							
+							<div className="notfound-actions">
+								<Link to="/" className="notfound-button notfound-button-primary">
+									<FontAwesomeIcon icon={faHome} />
+									<span>Back to Home</span>
+								</Link>
+								
+								<button 
+									onClick={() => window.history.back()} 
+									className="notfound-button notfound-button-secondary"
+								>
+									<FontAwesomeIcon icon={faArrowLeft} />
+									<span>Go Back</span>
+								</button>
+							</div>
+							
+							<div className="notfound-suggestions">
+								<p className="notfound-suggestions-title">Quick Links:</p>
+								<div className="notfound-links">
+									<Link to="/about" className="notfound-link">About</Link>
+									<Link to="/projects" className="notfound-link">Projects</Link>
+									<Link to="/articles" className="notfound-link">Articles</Link>
+									<Link to="/contact" className="notfound-link">Contact</Link>
+								</div>
+							</div>
+						</div>
+						
+						{/* Animated background elements */}
+						<div className="notfound-particles">
+							<div className="notfound-particle"></div>
+							<div className="notfound-particle"></div>
+							<div className="notfound-particle"></div>
+							<div className="notfound-particle"></div>
+							<div className="notfound-particle"></div>
 						</div>
 					</div>
 				</div>
